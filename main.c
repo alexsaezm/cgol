5e3f40431eb29cc6cf43514387717898e6aefc5c
Álex Sáez <alexsaezm@gmail.com>
2026-09-05 13:45:50 +0200

    Add a basic grid system
    This was fun to write but I'm not sure I really need to paint the grid
    at all.

--- main.c
+++ main.c
@@ -1,29 +1,84 @@
 #include "raylib.h"
 
+enum { ROWS = 50, COLS = 50 };
+
+void update(void) {
+	for (int row = 0; row < ROWS; row++) {
+		for (int col = 0; col < COLS; col++) {
+		}
+	}
+}
+
+void draw(bool showGrid) {
+	int width = GetScreenWidth();
+	int height = GetScreenHeight();
+
+	const int footerHeight = 40;
+	const int fontSize = 20;
+
+	int availableHeight = height - footerHeight;
+
+	float cellWidth = (float)width / COLS;
+	float cellHeight = (float)availableHeight / ROWS;
+	float cellSize = cellWidth < cellHeight ? cellWidth : cellHeight;
+
+	float gridWidth = COLS * cellSize;
+	float gridHeight = ROWS * cellSize;
+
+	float offsetX = (width - gridWidth) / 2.0f;
+	float offsetY = (availableHeight - gridHeight) / 2.0f;
+
+	BeginDrawing();
+	ClearBackground(BLACK);
+
+	if (showGrid) {
+		for (int row = 0; row <= ROWS; row++) {
+			float y = offsetY + row * cellSize;
+
+			DrawLine((int)offsetX, (int)y, (int)(offsetX + gridWidth), (int)y,
+					 LIGHTGRAY);
+		}
+
+		for (int col = 0; col <= COLS; col++) {
+			float x = offsetX + col * cellSize;
+
+			DrawLine((int)x, (int)offsetY, (int)x, (int)(offsetY + gridHeight),
+					 LIGHTGRAY);
+		}
+	}
+
+	const char *legend = "[G] Show/hide grid";
+	int legendWidth = MeasureText(legend, fontSize);
+
+	DrawText(legend, (width - legendWidth) / 2,
+			 height - footerHeight + (footerHeight - fontSize) / 2, fontSize,
+			 LIGHTGRAY);
+
+	EndDrawing();
+}
+
 int main(void) {
-  const int screenWidth = 800;
-  const int screenHeight = 450;
+	const int screenWidth = 800;
+	const int screenHeight = 450;
 
-  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-  InitWindow(screenWidth, screenHeight, "cgol");
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(screenWidth, screenHeight, "gcol");
+	MaximizeWindow();
 
-  SetTargetFPS(60);
-  while (!WindowShouldClose()) {
-    const char *text = "cgol";
+	SetTargetFPS(60);
 
-    int fontSize = 40;
-    int textWidth = MeasureText(text, fontSize);
+	bool showGrid = false;
 
-    int x = (GetScreenWidth() - textWidth) / 2;
-    int y = (GetScreenHeight() - fontSize) / 2;
+	while (!WindowShouldClose()) {
+		if (IsKeyPressed(KEY_G)) {
+			showGrid = !showGrid;
+		}
 
-    BeginDrawing();
-    ClearBackground(BLACK);
-    DrawText(text, x, y, fontSize, WHITE);
-    EndDrawing();
-  }
+		update();
+		draw(showGrid);
+	}
 
-  CloseWindow();
+	CloseWindow();
 
-  return 0;
+	return 0;
 }
